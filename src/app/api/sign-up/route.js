5d354d5e0b260ebf0f095657bc754e5188abb167
sarthu210 <sarthu102@gmail.com>
2024-06-19 11:19:27 +0530

    Sign-Up is completed

--- src/app/api/sign-up/route.js
+++ src/app/api/sign-up/route.js
@@ -1,15 +1,105 @@
+import bcrypt from "bcryptjs";
 import dbConnector from "@/lib/dbConnect";
 import { UserModel } from "@/model/User";
 import { sendVerificationEmail } from "@/helper/sendVerificationEmail";
-import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
 
 export async function POST(req){
     await dbConnector();
 
     try {
-        const {username, email, password} = await req.json()
+        const {username, email, password} = await req.json();
+        const ExistingVerifyeidUserByUsername = await UserModel.findOne({
+            username,
+            isVerifyed: true
+        })
 
-    } catch (error) {
+        if(ExistingVerifyeidUserByUsername){
+            return Response.json({
+                success: false,
+                message: "Username is already Taken"
+            },
+            {
+                status: 400
+            }
+        )
+        }
+
+        const ExistingUseramByEmail = await UserModel.findOne({email});
+
+        let verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+        if(ExistingUseramByEmail){
+            if(ExistingUseramByEmail.isVerifyed){
+                return Response.json({
+                    success: false,
+                    message: "Username is already exist with this email"
+                },
+                {
+                    status: 400
+                })
+            }
+            else{
+                const hashPassowrd = await bcrypt.hash(password, 10);
+                ExistingUseramByEmail.password = hashPassowrd;
+                ExistingUseramByEmail.verifyCode = verifyCode;
+                ExistingUseramByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);
+                await ExistingUseramByEmail.save();
+            }
+        
+        }
+        else{
+            const hashPassowrd = await bcrypt.hash(password, 10);
+            const expiaryDate = new Date();
+            expiaryDate.setHours(expiaryDate.getHours() + 1 );
+
+            const newUser = new UserModel({
+                username,
+                email,
+                password: hashPassowrd,
+                verifyCode,
+                verifyCodeExpiry: expiaryDate,
+                isVerifyed: false,
+                isAcceptingMessage: true,
+                messaage: []
+            })
+
+            await newUser.save();
+        }
+
+        //send verification email
+
+        const verificationEmailResposne = await sendVerificationEmail(
+            email,
+            username,
+            verifyCode
+        )
+
+        console.log(verificationEmailResposne);
+
+        if(!verificationEmailResposne.success){
+            return Response.json({
+                success: false,
+                message: verificationEmailResposne.message
+            },
+            {
+                status: 500
+            }
+        )
+        }
+
+        return Response.json({
+            success: true,
+            message: "User Successfuly Resgister successfuly. Please verify your account"
+        },
+        {
+            status: 201
+        }
+    )
+            
+    }
+        
+    catch (error) {
         console.log("Faild to sign-up" , error);
         return Response.json({
             success: true,
@@ -21,3 +111,4 @@ export async function POST(req){
     )
     }
 }
+
--- src/helper/sendVerificationEmail.js
+++ src/helper/sendVerificationEmail.js
@@ -3,13 +3,16 @@ import VerificationEmail from "../../emails/VerificationEmail";
 
 export async function sendVerificationEmail(email,username,verifycode){
     try{
-        await resend.emails.send({
-            from: 'Acme <onboarding@resend.dev>',
+        const res = await resend.emails.send({
+            from: 'onboarding@resend.dev',
             to: email,
             subject: 'Verification Code',
-            react: VerificationEmail(username,verifycode),
+            react: VerificationEmail({username, otp: verifycode}),
           });
+          console.log(res)
           return {success:true, message:"Email successfuly send"}
+
+        
     }
     catch(error){
         console.log("Falied to send verification email!");
